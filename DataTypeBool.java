6cefe0d62d9db8ace48edf385db6e695a11a1a7d
Monisha0503 <monishagnanaprakasam4@gmail.com>
2025-08-09 13:12:39 +0530

    Add files via upload

---
+++ DataTypeBool.java
@@ -0,0 +1,23 @@
+class DataTypeBool{//G.Monisha 2024503043
+public static void main(String[]args){
+Boolean a=true;
+System.out.println("a="+a);
+if(a)
+{
+System.out.println("it is true");
+}
+else
+{
+System.out.println("it is false");
+}
+int b=10;
+if(b>1)
+{
+System.out.println(" Greater");
+}
+else
+{
+System.out.println(" Smaller");
+}
+}
+}
